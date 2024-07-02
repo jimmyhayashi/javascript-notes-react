f80faecd339a10ddb4fbb4afb7b6a2ce6ec341f3
Jimmy Hayashihara <jimmyhayashi@live.com>
2024-07-02 09:33:45 +0200

    Created Form to Login the user

--- src/screens/auth/register/index.jsx
+++ src/screens/auth/register/index.jsx
@@ -1,8 +1,42 @@
 import React, { Fragment } from "react";
+import Header from "../../../components/header";
+import { Column, Section, Title, Container, Card } from "rbx";
+import logoImage from "../../../assets/images/logo.png";
+import '../../../styles/auth.scss';
+import RegisterForm from '../../../components/auth/register_form'
+
 
 const RegisterScreen = () => (
     <Fragment>
-        <h1>Register Screen</h1>
+        <Header />
+        <Section size="medium" className="auth">
+            <Container>
+                <Column.Group centered>
+                    <Column size={3}>
+                        <Card className="register-card">
+                            <Card.Content>
+                                <Section>
+                                    <Column.Group centered>
+                                        <Column size={12}>
+                                            <img src={logoImage} />
+                                        </Column>
+                                    </Column.Group>
+
+                                    <Column.Group>
+                                        <Column size={12}>
+                                            <Title size={6} className="has-text-grey has-text-centered">
+                                                Your notes on the cloud
+                                            </Title>
+                                            <RegisterForm />
+                                        </Column>
+                                    </Column.Group>
+                                </Section>
+                            </Card.Content>
+                        </Card>
+                    </Column>
+                </Column.Group>
+            </Container>
+        </Section>
     </Fragment>
 );  
 
