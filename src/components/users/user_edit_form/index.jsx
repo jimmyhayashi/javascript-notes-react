64550312409435ec3224aa693aa927c55f819e71
Jimmy Hayashihara <jimmyhayashi@live.com>
2024-07-24 06:58:37 +0200

    Added screen to edit user with forms

---
+++ src/components/users/user_edit_form/index.jsx
@@ -0,0 +1,78 @@
+import React, { Fragment, useState, useEffect } from "react";
+import { Button, Field, Control, Input, Help, Label } from "rbx";
+import { Column } from "rbx";
+import UsersService from "../../../services/user";
+
+function UsersEditForm() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [status, setStatus] = useState(null);
+
+    const initializerUser = async () => {
+        const user = await JSON.parse(localStorage.getItem('user'));
+        setEmail(user.email);
+        setName(user.name);
+    }
+
+    useEffect(() => {
+        initializerUser();
+    }, []);
+
+    const handleSubmit = async (evt) => {
+        evt.preventDefault();
+        try {
+            const user = await UsersService.update({ email: email, name: name });
+            setStatus("success");
+        } catch (error) {
+            setStatus("error");
+        }
+    };
+
+    return (
+        <Fragment>
+            <form onSubmit={handleSubmit}>
+                <Field>
+                    <Control>
+                        <Label className="has-text-grey">Full Name</Label>
+                        <Input
+                            type="text"
+                            value={name || ''}
+                            onChange={e => setName(e.target.value)}
+                            required
+                            name="name"
+                        />
+                    </Control>
+                </Field>
+                <Field>
+                    <Control>
+                        <Label className="has-text-grey">Email</Label>
+                        <Input
+                            type="email"
+                            value={email || ''}
+                            onChange={e => setEmail(e.target.value)}
+                            required
+                            name="email"
+                        />
+                    </Control>
+                </Field>
+                <Field>
+                    <Control>
+                        <Column.Group>
+                            <Column className="has-text-right">
+                                <Button color="custom-purple">Update</Button>
+                            </Column>
+                        </Column.Group>
+                    </Control>
+                </Field>
+                {status === "error" && (
+                    <Help color="danger">Problem in updating user</Help>
+                )}
+                {status === "success" && (
+                    <Help color="success">User updated successfully!</Help>
+                )}
+            </form>
+        </Fragment>
+    );  
+}
+
+export default UsersEditForm;
