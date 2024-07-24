64349aa6d3141888110af53ecfd54bd0ed10d75c
Jimmy Hayashihara <jimmyhayashi@live.com>
2024-07-24 07:00:03 +0200

    Create button to delete user

---
+++ src/components/users/user_user_delete/index.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { Button } from "rbx";
+import UsersService from "../../../services/user";
+import { Navigate } from "react-router-dom";
+
+function UserDelete() {
+    const [redirectToHome, setRedirectToHome] = useState(false);
+
+    const deleteUser = async () => {
+        if (window.confirm("Are you sure you want to delete your account?")) {
+            await UsersService.delete();
+            setRedirectToHome(true);
+        }
+    };  
+
+    if (redirectToHome) {
+        return <Navigate to="/" />;
+    }
+
+    return (
+        <Button color="danger" onClick={deleteUser}>Delete account</Button>
+    );
+}
+
+export default UserDelete;
+
